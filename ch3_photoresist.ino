37c86b8381d1cd1f2f8f8922803b0d97b9c3027d
Vitaly <makarov.1987@yandex.ru>
2022-03-13 00:01:35 +0300

    Update ch3_photoresist.ino

--- ch3_photoresist.ino
+++ ch3_photoresist.ino
@@ -1,10 +1,24 @@
-// Automatic Night Light
 
+//монитор значений фоторезистора
+const int photoresistorpin=0;
+int value=0;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  value = analogRead(photoresistorpin);
+  Serial.println(value);
+  delay(100);
+}
+
+/*
+//ночник
 const int LED=9;        // LED Anode on pin 9 (PWM)
 const int LIGHT=0;       // Light Sensor on Analog Pin 0
 const int MIN_LIGHT=200; // Minimum Expected light value
 const int MAX_LIGHT=900; // Maximum Expected Light value
-int val = 0;             // Variable to hold the analog reading
+int value = 0;             // Variable to hold the analog reading
 
 void setup()
 {
@@ -14,10 +28,11 @@ void setup()
 
 void loop()
 {
-  val = analogRead(LIGHT);                      // Read the light sensor
-  val = map(val, MIN_LIGHT, MAX_LIGHT, 255, 0); // Map the light reading
-  val = constrain(val, 0, 255);                 // Constrain light value
-  analogWrite(LED, val);                       // Control the White LED
-  Serial.println(val);
+  value = analogRead(LIGHT);                      // Read the light sensor
+  value = map(value, MIN_LIGHT, MAX_LIGHT, 255, 0); // Map the light reading
+  value = constrain(value, 0, 255);                 // Constrain light value
+  analogWrite(LED, value);                       // Control the White LED
+  Serial.println(value);
   delay(1000);
 }
+*/
